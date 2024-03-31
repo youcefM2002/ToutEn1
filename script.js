d28b89704ef099ab68a4687042edf246e2eb7d61
youcefM2002 <160131006+youcefM2002@users.noreply.github.com>
2024-03-31 20:51:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById("toggleButton").addEventListener("click", function() {
+  var sidebar = document.getElementById("sidebar");
+  var content = document.getElementById("content");
+  var toggleButton = document.getElementById("toggleButton"); // Référence au bouton
 
+  if (!sidebar.classList.contains("open")) { // Si le sidebar est fermé
+    sidebar.classList.add("open");
+    content.classList.add("shifted");
+    toggleButton.innerText = "Fermer"; // Met à jour le texte du bouton
+  } else { // Si le sidebar est ouvert
+    sidebar.classList.remove("open");
+    content.classList.remove("shifted");
+    toggleButton.innerText = "Toutes"; // Met à jour le texte du bouton
+  }
+});
